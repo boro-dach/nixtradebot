05b0a2947c26105847d8c52791a7dbc5689315a9
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-09-16 21:07:54 +0200

    fix types

--- web/shared/providers/theme-provider.tsx
+++ web/shared/providers/theme-provider.tsx
@@ -2,10 +2,14 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes";
 
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
+// Define the component's props as an intersection of ThemeProviderProps
+// and an object containing the children prop.
+type Props = ThemeProviderProps & {
+  children: React.ReactNode;
+};
+
+export function ThemeProvider({ children, ...props }: Props) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
