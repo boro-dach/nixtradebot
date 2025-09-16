caa2052f093d2de520c036cfdef92115318ac122
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-09-16 23:08:29 +0200

    backend auth

--- web/shared/providers/theme-provider.tsx
+++ web/shared/providers/theme-provider.tsx
@@ -4,8 +4,6 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes";
 
-// Define the component's props as an intersection of ThemeProviderProps
-// and an object containing the children prop.
 type Props = ThemeProviderProps & {
   children: React.ReactNode;
 };
