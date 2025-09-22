92ad0006148f1d1659b2bfda02066bd68229c0e1
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-09-22 07:45:43 -0700

    fix

--- web/entities/transaction/api/create.ts
+++ web/entities/transaction/api/create.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export async function createTransaction(values: any) {
-    try {
-        const response = await axios.post
-    }
+  try {
+    const response = await axios.post(
+      "http://localhost:5000/transaction/create",
+      values
+    );
+  } catch (err) {}
 }
